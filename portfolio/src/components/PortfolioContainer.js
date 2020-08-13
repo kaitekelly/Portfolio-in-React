a66612c371f023291e22080c9a176d809d254c72
Kathleen Kelly <murphy.kaite@gmail.com>
2020-08-12 23:53:18 -0700

    updated porfoliocontainer

---
+++ portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+class Portfolio extends Component {
+
+}
+
+export default Portfolio;
