bea2b650a4e7814ad0ec3edb295293108176494a
Kathleen Kelly <murphy.kaite@gmail.com>
2020-08-13 00:38:38 -0700

    added code to components

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Contact = () => (
-
+  <div>
+      <h1>Contact Me</h1>
+      <p>
+          
+      </p>
+  </div>
 );
 
 export default Contact;
--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 const Home = () => (
+  <div>
+      <h1>Kaite Kelly's Portfolio</h1>
+  </div>
 
 );
 
