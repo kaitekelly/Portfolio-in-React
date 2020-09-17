cbf1a93a10790b548819737eb008e7fbf8f9b440
Kathleen Kelly <murphy.kaite@gmail.com>
2020-09-16 22:03:17 -0700

    Updates

---
+++ precache-manifest.fa4a7e9eb4b9b05d7b79cf5c81ff8e44.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42efd8b5c1b00777a097f19dac32229",
+    "url": "/Portfolio-in-React/index.html"
+  },
+  {
+    "revision": "3b1cd068b83739032a03",
+    "url": "/Portfolio-in-React/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "94bf7ef820c5e58b4a2b",
+    "url": "/Portfolio-in-React/static/css/main.f2a85c4d.chunk.css"
+  },
+  {
+    "revision": "3b1cd068b83739032a03",
+    "url": "/Portfolio-in-React/static/js/2.eea6bf2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio-in-React/static/js/2.eea6bf2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94bf7ef820c5e58b4a2b",
+    "url": "/Portfolio-in-React/static/js/main.2a782192.chunk.js"
+  },
+  {
+    "revision": "800243d85434ad86f2a1",
+    "url": "/Portfolio-in-React/static/js/runtime-main.9585c088.js"
+  },
+  {
+    "revision": "e16a39e4c37f5765402a6fd63bac77f3",
+    "url": "/Portfolio-in-React/static/media/brewr-img.e16a39e4.jpg"
+  },
+  {
+    "revision": "006add660cd349f3a0f85a718052960c",
+    "url": "/Portfolio-in-React/static/media/burger-wish-image.006add66.jpg"
+  },
+  {
+    "revision": "8b935d55581dd9ae295c6a07ae13e864",
+    "url": "/Portfolio-in-React/static/media/employee-database.8b935d55.jpg"
+  },
+  {
+    "revision": "215668e176b1e949889fb0b32c01b39f",
+    "url": "/Portfolio-in-React/static/media/express-note-taker-image.215668e1.jpg"
+  },
+  {
+    "revision": "1fdd2cdc8b81dbcefeff723fe4589826",
+    "url": "/Portfolio-in-React/static/media/find-next-gig.1fdd2cdc.jpg"
+  },
+  {
+    "revision": "9c7e2ccfafe42c272abb030800694c03",
+    "url": "/Portfolio-in-React/static/media/password-generator-color.9c7e2ccf.jpg"
+  },
+  {
+    "revision": "594119c1aec819767802578f9a1d877d",
+    "url": "/Portfolio-in-React/static/media/readme-generator.594119c1.jpg"
+  },
+  {
+    "revision": "3c8f07d482b6d75613dad373b4472d4c",
+    "url": "/Portfolio-in-React/static/media/team-roster-image.3c8f07d4.jpg"
+  },
+  {
+    "revision": "3c59e645c118bae1f24b064aff1026c6",
+    "url": "/Portfolio-in-React/static/media/weather-dashboard.3c59e645.jpg"
+  },
+  {
+    "revision": "7f148d20a9b2235d37703d67fdd6344a",
+    "url": "/Portfolio-in-React/static/media/workday-scheduler.7f148d20.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Portfolio-in-React/precache-manifest.fa4a7e9eb4b9b05d7b79cf5c81ff8e44.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio-in-React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
