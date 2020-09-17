e37166f3f6338bd1e14dceb2acaeb63c4ff54a9c
Kathleen Kelly <murphy.kaite@gmail.com>
2020-09-17 14:26:26 -0700

    deployment 23

---
+++ build/precache-manifest.ac1861a797f12b134cd4e200981663f0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31431cd51b49f87c76eb152da96b1a3a",
+    "url": "/Portfolio-in-React/index.html"
+  },
+  {
+    "revision": "6e8e2bed80c6fe5f8d17",
+    "url": "/Portfolio-in-React/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "3fbdf26dc3717ed43324",
+    "url": "/Portfolio-in-React/static/css/main.7e6cd68d.chunk.css"
+  },
+  {
+    "revision": "6e8e2bed80c6fe5f8d17",
+    "url": "/Portfolio-in-React/static/js/2.f0000ff6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio-in-React/static/js/2.f0000ff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fbdf26dc3717ed43324",
+    "url": "/Portfolio-in-React/static/js/main.f16ffca1.chunk.js"
+  },
+  {
+    "revision": "800243d85434ad86f2a1",
+    "url": "/Portfolio-in-React/static/js/runtime-main.9585c088.js"
+  },
+  {
+    "revision": "7de2af0796fc471f052f17b7588947da",
+    "url": "/Portfolio-in-React/static/media/IMG_0414_a.7de2af07.jpeg"
+  },
+  {
+    "revision": "e16a39e4c37f5765402a6fd63bac77f3",
+    "url": "/Portfolio-in-React/static/media/brewr-img.e16a39e4.jpg"
+  },
+  {
+    "revision": "006add660cd349f3a0f85a718052960c",
+    "url": "/Portfolio-in-React/static/media/burger-wish-image.006add66.jpg"
+  },
+  {
+    "revision": "8b935d55581dd9ae295c6a07ae13e864",
+    "url": "/Portfolio-in-React/static/media/employee-database.8b935d55.jpg"
+  },
+  {
+    "revision": "215668e176b1e949889fb0b32c01b39f",
+    "url": "/Portfolio-in-React/static/media/express-note-taker-image.215668e1.jpg"
+  },
+  {
+    "revision": "1fdd2cdc8b81dbcefeff723fe4589826",
+    "url": "/Portfolio-in-React/static/media/find-next-gig.1fdd2cdc.jpg"
+  },
+  {
+    "revision": "9c7e2ccfafe42c272abb030800694c03",
+    "url": "/Portfolio-in-React/static/media/password-generator-color.9c7e2ccf.jpg"
+  },
+  {
+    "revision": "594119c1aec819767802578f9a1d877d",
+    "url": "/Portfolio-in-React/static/media/readme-generator.594119c1.jpg"
+  },
+  {
+    "revision": "3c8f07d482b6d75613dad373b4472d4c",
+    "url": "/Portfolio-in-React/static/media/team-roster-image.3c8f07d4.jpg"
+  },
+  {
+    "revision": "3c59e645c118bae1f24b064aff1026c6",
+    "url": "/Portfolio-in-React/static/media/weather-dashboard.3c59e645.jpg"
+  },
+  {
+    "revision": "7f148d20a9b2235d37703d67fdd6344a",
+    "url": "/Portfolio-in-React/static/media/workday-scheduler.7f148d20.jpg"
+  }
+]);
