9092acf055ef74506e6be85526f9791e1b599e7c
Kathleen Kelly <murphy.kaite@gmail.com>
2020-10-30 20:53:39 -0700

    update resume link to google doc

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -11,8 +11,8 @@ export default function Resume() {
     <div className="Resume">
       <Container className="themed-container">
       <ReactEmbedGist gist="kaitekelly/2982da5372b914e9ba9f421808fa1e8f"/>
-      <h1>Click link to view a pdf of my resume</h1>
-      <a href="https://docs.google.com/document/d/1srAfcL5tCjcfR3c9VXBsIIeT3fYwDBi85lHKJk6ZMR0/edit?usp=sharing" target="blank">View Resume pdf</a>
+      {/* <h1>Click link to view a pdf of my resume</h1> */}
+      <a href="https://docs.google.com/document/d/1srAfcL5tCjcfR3c9VXBsIIeT3fYwDBi85lHKJk6ZMR0/edit?usp=sharing" target="blank" className="resume-pdf">Click here to view resume as pdf</a>
       </Container>
     </div>
 
