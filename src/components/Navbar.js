e987258e39b10163efd88ea93f2a6993911a05d8
kaitekelly <murphy.kaite@gmail.com>
2021-06-04 13:06:51 -0700

    navbar style update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = ({ name }) => {
     <div>
       <ul>
         <li>
-        <Link to="/" className="link">Home</Link> 
+        <Link to="/" className="link">About</Link> 
         </li>
         <li>
         <Link to="/gallery" className="link">Projects</Link>
